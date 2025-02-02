3f62940ef294e71540d8e5045eb808447d9b1b6b
Jack Walsh <123195925+walshjack1219@users.noreply.github.com>
2025-02-02 18:15:47 -0500

    Add files via upload
    added the polesocks file from arduino

---
+++ poleSocks.ino
@@ -0,0 +1,20 @@
+/*                                    WAIT!!!!
+before anything else, we need to include the necessary libraries to work with our components, the MAX30102 and the GME 12864-17
+*/
+
+
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
